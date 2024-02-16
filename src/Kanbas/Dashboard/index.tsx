db6771b1103bd94df8ed6c983212f41a8383acdd
dthach41 <thachderek1128@gmail.com>
2024-02-16 09:58:42 -0500

    Added  a course-todo.json

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { courses } from "../Database";
+import "./index.css"; 
 
 function Dashboard() {
     return (
@@ -10,16 +11,16 @@ function Dashboard() {
             <hr />
             <h2>Published Courses (7)</h2>
             <hr />
-            <div className="row" style={{fontSize: "13"}}>
+            <div className="row" style={{fontSize: "8"}}>
                 <div className="row row-cols-1 row-cols-md-5 g-4">
                     {courses.map((course) => (
-                        <div className="col" style={{ width: "270px" }}>
+                        <div className="col" style={{ width: "270px"}}>
                             <div className="card">
                                 <img src={course.image} className="card-img-top" style={{ height: "140px" }} />
                                 <div className="card-body">
                                     <Link className="card-title" to={`/Kanbas/Courses/${course._id}`}
                                         style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                                        {course.number + " " + course.name}</Link>
+                                        <div className="course-info">{course.number + " " + course.name}</div> </Link>
                                     <p className="card-text">{course._id + " " + course.startDate}</p>
                                     <Link to={`/Kanbas/Courses/${course._id}`} className="btn btn-primary"> Go </Link>
                                 </div>
