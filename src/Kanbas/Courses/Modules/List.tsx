5a6482182831cec8b9db0a8baf17f383d66b4b7e
dthach41 <thachderek1128@gmail.com>
2024-03-15 00:45:41 -0400

    Working on Editing Module, view comment to see where left off

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./index.css";
 import { modules } from "../../Database";
-import { FaEllipsisV, FaCheckCircle, FaPlusCircle, FaPlus } from "react-icons/fa";
+import { FaEllipsisV, FaCheckCircle, FaPlusCircle, FaPlus, FaMinus, FaMinusCircle, FaPen } from "react-icons/fa";
 import { useParams } from "react-router";
 
 function ModuleList() {
@@ -15,9 +15,36 @@ function ModuleList() {
         name: "New Module",
         description: "New Description",
         courseId: courseId,
+        _id: 0
     });
 
-    
+    const addModule = (module: any) => {
+        const newModule = {
+            ...module,
+            _id: new Date().getTime().toString()
+        };
+        const newModuleList = [newModule, ...moduleList];
+        setModuleList(newModuleList);
+    };
+
+
+    const deleteModule = (moduleId: string) => {
+        const newModuleList = moduleList.filter(
+            (module) => module._id !== moduleId);
+        setModuleList(newModuleList);
+    };
+
+    const updateModule = () => {
+        const newModuleList = moduleList.map((m) => {
+            if (m._id === module._id) {
+                return module;
+            } else {
+                return m;
+            }
+        });
+        setModuleList(newModuleList);
+    };
+
 
 
     return (
@@ -36,9 +63,13 @@ function ModuleList() {
 
             <ul className="list-group wd-modules" style={{ marginRight: "15px" }}>
 
+                {/* Modules Editing Buttons */}
                 <div className="flex-buttons-container">
-                    <button className="btn add-button" >Add <FaPlus /> </button>
+                    <button className="btn add-button" onClick={() => { addModule(module) }}>Add <FaPlus /> </button>
+                    <button className="btn add-button" onClick={updateModule}>Update <FaPen/> </button>
+
                 </div>
+
 
                 <input value={module.name} className="form-control module-menu-input"
                     onChange={(e) => setModule({
@@ -54,21 +85,29 @@ function ModuleList() {
                 <hr style={{marginBottom: "25px"}}/>
 
 
-                {modulesList.filter((module) => module.courseId === courseId).map((module, index) => (
+                {moduleList.filter((module) => module.courseId === courseId).map((module, index) => (
                     <li key={index} className="list-group-item" onClick={() => setSelectedModule(module)}>
                         <div style={{ marginBottom: "10px" }} >
                             <FaEllipsisV className="me-2" />
-                            {module.name}
+                            {module.description === "" ? (
+                                // WORKING ON THIS RIGHT HERE ---------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+                                <>{module.name}</>
+                            ) : (
+                                <>{module.name} <br/> {module.description}</>
+                            )}
+
                             <span className="float-end">
                                 <FaCheckCircle className="text-success" />
                                 <FaPlusCircle className="ms-2" />
                                 <FaEllipsisV className="ms-2" />
+                                <button className="module_small_btn ms-1" style={{color: 'red'}} onClick={() => deleteModule(module._id)}><FaMinusCircle /> </button>
+                                <button className="module_small_btn ms-2" onClick={(event) => { setModule(module); }}> <FaPen/></button>
                             </span>
                         </div>
 
                         {selectedModule._id === module._id && (
                             <ul className="list-group">
-                                {module.lessons?.map((lesson) => (
+                                {module.lessons?.map((lesson: { name: string | number | boolean }) => (
                                     <li className="list-group-item">
                                         <FaEllipsisV className="me-2" />
                                         {lesson.name}
@@ -82,6 +121,7 @@ function ModuleList() {
                         )}
                     </li>
                 ))}
+                
             </ul>
         </>
     );
