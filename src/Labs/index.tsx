c7e330609fcd1e09ad387e99698e818654013291
dthach41 <thachderek1128@gmail.com>
2024-03-18 13:03:53 -0400

    first commit

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,6 +6,7 @@ import { Routes, Route, Navigate } from "react-router";
 import Assignment4 from "./a4";
 import { Provider } from "react-redux";
 import store from "./store";
+import Assignment5 from "./a5";
 
 function Labs() {
     return (
@@ -13,11 +14,13 @@ function Labs() {
             <div className="container-fluid">
                 <h1>Labs</h1>
                 <Nav />
-                <Link to="/Labs/a3">Assignment 3</Link> |
-                <Link to="/Labs/a4">Assignment 4</Link>
+                <Link to="/Labs/a3" style={{ margin: '5px' }}>Assignment 3</Link> 
+                <Link to="/Labs/a4" style={{ margin: '5px' }}>Assignment 4</Link>
+                <Link to="/Labs/a5" style={{ margin: '5px' }}>Assignment 5</Link>
                 <Routes>
                     <Route path="/a3/*" element={<Assignment3 />} />
                     <Route path="/a4" element={<Assignment4 />} />
+                    <Route path="/a5" element={<Assignment5 />} />
                 </Routes>
             </div>
         </Provider>
