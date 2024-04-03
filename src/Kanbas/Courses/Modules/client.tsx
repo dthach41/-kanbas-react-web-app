22cac1c07600fd443da2240952747b08d8fdbd4f
dthach41 <thachderek1128@gmail.com>
2024-04-02 23:25:11 -0400

    final

--- src/Kanbas/Courses/Modules/client.tsx
+++ src/Kanbas/Courses/Modules/client.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-const COURSES_API = "http://localhost:4000/api/courses";
-const MODULES_API = "http://localhost:4000/api/modules";
+const COURSES_API = "https://kanbas-node-server-app.onrender.com/api/courses";
+const MODULES_API = "https://kanbas-node-server-app.onrender.com/api/modules";
 
 export const findModulesForCourse = async (courseId?: string) => {
     const response = await axios
