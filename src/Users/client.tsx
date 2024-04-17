4386d8b31c73ec032630de14f2176c6d04c73001
dthach41 <thachderek1128@gmail.com>
2024-04-16 20:22:16 -0400

    final a6

--- src/Users/client.tsx
+++ src/Users/client.tsx
@@ -1,8 +1,10 @@
 import axios from "axios";
+axios.defaults.withCredentials = true
+
 export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 export const USERS_API = `${BASE_API}/api/users`;
 
-axios.defaults.withCredentials = true
+
 
 export interface User {
     _id: string; username: string; password: string; role: string;
